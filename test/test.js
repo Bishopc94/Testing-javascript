78dfab753373ed2f1259ec7ce97cd9b819b61452
Christopher Bishop <bishopc@alfredstate.edu>
2015-11-11 21:32:31 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-var assert = require("assert")
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
-=======
 var assert = require('assert');
 var Fizz = require('../fizzBuzz.js').fizz;
 describe('FizzBuzz', function() {
@@ -19,4 +8,3 @@ describe('FizzBuzz', function() {
   // Print Buzz for numbers Divisible by 5
   // Print FizzBuzz for numbers Divisible by 3 and 5
 });
->>>>>>> 423c43380c1100647be334822d96f38543b814b9

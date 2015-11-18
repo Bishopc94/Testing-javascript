4f200901e6714abcae83e585cbd396ab79e307a8
Christopher Bishop <bishopc@alfredstate.edu>
2015-11-18 10:20:47 -0500

    Update fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,6 +1,43 @@
-function FizzBuzz(){
-  return 0;
+/*
+*   Write a program that prints the numbers from 1 to 100.
+*   But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+*   For numbers which are multiples of both three and five print “FizzBuzz”.
+*   Use % === 0
+*/
+function FizzBuzz(num){
+  if(isGreaterThanOne(num)){
+    if(isFizzBuzz(num)){
+      return 'Fizz Buzz';
+    }else if(isFizz(num)){
+      return 'Fizz';
+    }else if(isBuzz(num)){
+      return 'Buzz';
+    }else {
+      return num;
+    }
+  }else {
+    return false;
+  }
 }
-exports.main = {
-  FizzBuzz: FizzBuzz
-};
+function isGreaterThanOne(num){
+  if(num >= 1){
+    return true;
+  }
+}
+function isFizz(num){
+  return num % 3 == 0;
+}
+function isBuzz(num){
+  return num % 5 == 0;
+}
+function isFizzBuzz(num){
+  return num % 15 == 0;
+}
+(function Print(){
+  for(var i = 0; i <= 100; i++){
+    if(isGreaterThanOne(i)){
+      console.log(FizzBuzz(i));
+    }
+  }
+})();
+module.exports = FizzBuzz;
